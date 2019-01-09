ce3a5e52bda9a2f3a9158a9347afb91b3eb45d2c
Gabriel Barrantes <gabobj94@hotmail.com>
2019-01-09 17:55:06 -0600

    Changed models to english

--- src/padronelectoral/models.py
+++ src/padronelectoral/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 # Create your models here.
-class Provincia(models.Model):
-    nombreProvincia= models.CharField(max_length=10)
+class Province(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
 
 class Canton(models.Model):
-    nombreCanton=models.CharField(max_length=20)
-    provincia=models.ForeignKey(Provincia,on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    idProvince = models.ForeignKey(Province,on_delete=models.CASCADE)
 
-class Distrito(models.Model):
-    codelec=models.PositiveIntegerField(primary_key=True,validators=[MaxValueValidator(999999)])
-    nombreProvincia = models.CharField(max_length=34)
+class District(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    codelec=models.PositiveIntegerField(validators=[MaxValueValidator(999999)])
+    name = models.CharField(max_length=34)
     canton=models.ForeignKey(Canton, on_delete=models.CASCADE)
 
 class Elector(models.Model):
-    cedula=models.PositiveIntegerField(primary_key=True, validators=[MaxValueValidator(999999999)])
-    codelec=models.ForeignKey(Distrito, on_delete=models.CASCADE)
-    sexo=models.SmallIntegerField()
-    fecha_cad=models.DateField()
-    junta=models.IntegerField(validators=[MaxValueValidator(999999)])
-    nombre=models.CharField(max_length=30)
-    apellido1=models.CharField(max_length=26)
-    apellido2 = models.CharField(max_length=26)
+    idCard = models.PositiveIntegerField(primary_key=True, validators=[MaxValueValidator(999999999)])
+    codelec = models.ForeignKey(District, on_delete=models.CASCADE)
+    gender = models.SmallIntegerField()
+    cad_date = models.DateField()
+    junta = models.IntegerField(validators=[MaxValueValidator(999999)])
+    name = models.CharField(max_length=30)
+    lastname1 = models.CharField(max_length=26)
+    lastname2 = models.CharField(max_length=26)
