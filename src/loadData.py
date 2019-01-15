e8b3af438a613a6423a6ed0de0612bd38ac66a1e
MiguelMendez <miguel1796@live.com>
2019-01-14 21:36:57 -0600

    load Province, Canton and districts querys

--- src/loadData.py
+++ src/loadData.py
@@ -10,8 +10,6 @@ def loadData(path, max_len):
     linesList = txtFile.readlines()
     dictionaryList = []
 
-    print(len(linesList))
-
     for line in linesList:
         dictionaryToSave = splitLine(line)
         dictionaryList.append(dictionaryToSave)
@@ -38,14 +36,10 @@ def dictionaryListToValues(dictionaryList):
 
 
 def saveLine(values):
-    # columns = ', '.join(dictionaryToSave.keys())
     sql = "INSERT INTO %s (idCard, gender, cad_date, board, fullName, name, lastname1, lastname2, codelec_id) VALUES %s;" % (
         'padronelectoral_elector', values)
     sqlFile = open("querys.sql", "a")
     sqlFile.write(sql)
-
-
-#  cursor.execute(sql, dictionaryToSave.values())
 
 def splitLine(line):
     split = line.split(",")
@@ -63,11 +57,64 @@ def splitLine(line):
     }
     return orderedInfo
 
+provinceIdAux = 0
+cantonIdAux = 0
+def splitLineCodelec(line):
+    global provinceIdAux
+    global cantonIdAux
+    split = line.split(",")
+    codelec = split[0]
+    provinceData = {'id': codelec[0],
+                   'name':split[1]}
+    cantonData = {'id': codelec[1:3],
+                  'name': split[2],
+                  'province': codelec[0]}
+    districtData = {'id': codelec[3:],
+                    'codelec': codelec,
+                    'name': split[3].strip(),
+                    'canton': codelec[1:3]}
+
+    #this if is needed because in case that not exist, the file will append repeat data many times
+    if(provinceIdAux!=codelec[0]):
+        queryProvince = createQuerys('padronelectoral_province', provinceData)
+        sqlFileP = open("querysProvince.sql", "a")
+        sqlFileP.write(queryProvince)
+        provinceIdAux = codelec[0]
+
+    if(cantonIdAux!=codelec[1:3]):
+        queryCanton = createQuerys('padronelectoral_canton', cantonData)
+        sqlFileC = open("querysCanton.sql", "a")
+        sqlFileC.write(queryCanton)
+        cantonIdAux = codelec[1:3]
+
+    queryDistrict = createQuerys('padronelectoral_district', districtData)
+    sqlFileD = open("querysDistrict.sql", "a")
+    sqlFileD.write(queryDistrict)
+
+def createQuerys(table, dictionaryData):
+    keysTuple = ', '.join(dictionaryData.keys())
+    valuesTuple = ', '.join(dictionaryData.values())
+    return "Insert Into %s (%s) Values (%s) ;" % (table, keysTuple, valuesTuple)
+
+
+def loadDataCodelec(path):
+    txtFile = ""
+    try:
+        txtFile = open(path)
+    except:
+        print("Can't read this path..")
+        return
+    linesList = txtFile.readlines()
+    for line in linesList:
+        splitLineCodelec(line)
+
 
 def defineParameters():
     path = '/home/miguelmendezrojas/Descargas/padron_completo/PADRON_COMPLETO.txt'
+    pathDistelec = '/home/miguelmendezrojas/Descargas/padron_completo/Distelec.txt'
     round = 15
-    loadData(path, round)
 
+    loadDataCodelec(pathDistelec)
+    loadData(path, round)
 
 defineParameters()
