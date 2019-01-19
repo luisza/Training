9d67a39d34ac8ec8a52252395b27cdfe88d1fcb7
Alejandro <alegamboaba.17@gmail.com>
2019-01-19 02:15:24 +0100

    template to add new elector

--- src/padronelectoral/urls.py
+++ src/padronelectoral/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search', views.get_electors, name='get_electors'),
     path('stats/province/<int:pk>', views.get_province_data, name='get_province_data'),
     path('stats/canton/<int:pk>', views.get_canton_data, name='get_canton_data'),
-    path('stats/district/<int:pk>', views.get_district_data, name='get_district_data')
+    path('stats/district/<int:pk>', views.get_district_data, name='get_district_data'),
+    path('newelector',views.createElector,name='new_elector')
     #  path('stats/<str:pk>/', views.DeleteDistrict, name='deleteDistrict'),
 ]
