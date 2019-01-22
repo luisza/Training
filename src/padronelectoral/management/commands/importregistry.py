e2cc87f1e993eb5e81a41150a3a1604f9d43253f
Luis Zárate M <luis.zarate@solvosoft.com>
2019-01-21 22:22:13 -0600

    Docs and logs

--- src/padronelectoral/management/commands/importregistry.py
+++ src/padronelectoral/management/commands/importregistry.py
@@ -1,13 +1,14 @@
 import argparse
+import time
+import logging
 
 from django.core.management.base import BaseCommand
-# from padronelectoral import loadData
 from django.db import connection
 from django.db.models import signals
-import time
 from padronelectoral.models import District, Province, Canton, Elector
 from padronelectoral.signals import update_district
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Import CR registry '
@@ -15,7 +16,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         """
-        Include extra parameters to command pipeline, define tuncate value to 40000.
+        Include extra parameters to command pipeline, define truncate value to 40000.
         Allow no clean parameter to prevent data clean.
 
 
@@ -42,16 +43,27 @@ class Command(BaseCommand):
             help='',
         )
 
-
     def calculate_speed(self, func, message, *args):
+        """
+        Calculate the time spend by a function call
+
+        :param func:  Function to call
+        :param message: Message to print when time speed is available
+        :param args:  Arguments to pass to functions
+        """
         start = time.time()
         func(*args)
         end = time.time()
         print("Complete %s in "%message, end - start, " s")
 
     def handle(self, *args, **options):
+        """
+        Main call, process management callback and import all registry
+        """
         start = time.time()
+        logger.info("Stating import of registry")
         if not options['noclean']:
+            logger.info("Cleanup datatables")
             self.calculate_speed(self.clean_tables, "Cleaning datatables")
 
         self.calculate_speed(self.import_districts,"Importing districts", options)
@@ -61,15 +73,28 @@ class Command(BaseCommand):
         print("Complete in ", end - start, " s")
 
     def clean_tables(self):
+        """
+        Delete all data on database if exists.
+        """
         print("Deleting all registry data")
         with connection.cursor() as cursor:
+            logger.debug("Execute 'TRUNCATE `padronelectoral_elector`' ")
             # Delete in raw for optimization
             cursor.execute('TRUNCATE `padronelectoral_elector`')
+
         # Using cascade aproach to delete other tables
         print(Province.objects.all().delete())
 
 
     def get_canton(self, province, name, code):
+        """
+        Return a canton based on the name and code passed to function.
+
+        :param province: province name
+        :param name: canton name
+        :param code: district code
+        :return: Django Canton model
+        """
         cantonid = code[:3]
         if cantonid not in self.cantons:
             province, created = Province.objects.get_or_create(
@@ -84,6 +109,11 @@ class Command(BaseCommand):
         return self.cantons[cantonid]
 
     def import_districts(self, options):
+        """
+        Import all districts form diselect.txt file.
+
+        :param options: Argparse arguments (required diselect)
+        """
         print("Importing Districts ")
         distrits = []
         for line in options['diselect'].readlines():
@@ -97,8 +127,14 @@ class Command(BaseCommand):
             ))
         District.objects.bulk_create(distrits)
         print("Importing %d districts " % (len(distrits)))
+        logger.info("Importing %d districts " % (len(distrits)))
 
     def get_values_from_file(self, options):
+        """
+        Extract some amount of  values based on truncate parameter on options, yield data with sql structure to insert
+        into database.
+        :param options:  Argparse arguments (required registry)
+        """
         max_element = options['truncate']
         count = 0
         values = ""
@@ -127,10 +163,16 @@ class Command(BaseCommand):
                 cursor.execute(sql%(values,) )
                 count += options['truncate']
                 print("Importing %d electors"%count , end='')
+                # revert the car to before line on console
                 print('\r', end='')
         print("")
+        logger.info("Importing %d electors " % (count))
 
     def calculate_stats(self):
+        """
+        Calculate the stats base on imported data
+        """
         print("Doing stats")
         for dist in District.objects.all():
             update_district(dist)
+        logger.info("Calculate stats to %d districts " % (District.objects.all().count()))
